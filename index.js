e64572325b1b706fc46838b3cf5c170fe9e5a2b6
Noor-Afik-Jalaludeen-A <noorafikjalaludeen2204@gmail.com>
2024-09-21 22:54:33 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,41 @@
+var popupoverlay=document.querySelector(".popup-overlay")
+var popupbox=document.querySelector(".popup-box")
+var addpopupbutton=document.getElementById("add-popup-button")
+
+addpopupbutton.addEventListener("click",function(){
+    popupoverlay.style.display="block"
+    popupbox.style.display="block"
+    
+});
+
+var cancelpopup=document.getElementById("cancel-popup")
+cancelpopup.addEventListener("click",function(){
+    popupbox.style.display="none"
+    popupoverlay.style.display="none"
+})
+
+var container=document.querySelector(".container")
+var addbook = document.getElementById("add-book")
+var booktitle=document.getElementById("book-title-input")
+var bookauthor=document.getElementById("book-author-input")
+var bookdescription =document.getElementById("book-description-input")
+
+addbook.addEventListener("click",function(){
+    var div=document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h2>${booktitle.value}</h2>
+            <h5>${bookauthor.value}</h5>
+            <p>${bookdescription.value}</p>
+            <button id="del-notes">Delete</button>`
+    container.append(div)
+    popupbox.style.display="none"
+    popupoverlay.style.display="none"
+    booktitle.value=""
+    bookauthor.value=""
+    bookdescription.value=""
+})
+var delelete=document.getElementById("del-notes")
+
+delelete.addEventListener("click",function(event){
+event.target.parentElement.remove()
+})
